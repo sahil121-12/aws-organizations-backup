27514be102a6beb5991d56f96607d474c5ace269
sahil1202-12 <sahilsahni1819@gmail.com>
2023-06-08 10:40:16 +0530

    second commit

--- EnabledPolices/ai_policies.py
+++ EnabledPolices/ai_policies.py
@@ -1,5 +1,90 @@
+# import boto3
+# import json
+
+# def list_ai_policies():
+#     # Create a Boto3 client for AWS Organizations
+#     client = boto3.client('organizations')
+
+#     # Retrieve the list of AI service opt-out policies
+#     response = client.list_policies(Filter='AISERVICES_OPT_OUT_POLICY')
+
+#     # Extract the list of policies
+#     ai_policies = response['Policies']
+
+#     # Specify the output folder where JSON files will be stored
+#     output_folder = '/home/sahil/Documents/All_detail/output/Policies/ai_policies'
+
+#     # Get the details of each AI policy
+#     for ai_policy in ai_policies:
+#         policy_id = ai_policy['Id']
+#         policy_name = ai_policy['Name']
+
+#         # Retrieve the policy content
+#         policy_response = client.describe_policy(PolicyId=policy_id)
+#         policy_content_str = policy_response['Policy']['Content']
+
+#         # Create a separate JSON file for each AI policy in the output folder
+#         policy_file_name = f"{output_folder}/{policy_name}.json"
+
+#         # Load the policy content as JSON to format it
+#         policy_content = json.loads(policy_content_str)
+
+#         # Write the formatted policy content to the JSON file
+#         with open(policy_file_name, 'w') as file:
+#             json.dump(policy_content, file, indent=4)
+
+#         print(f"JSON file generated for policy '{policy_name}'.")
+
+
+
+
+
+# import boto3
+# import json
+# import os
+
+# def list_ai_policies():
+#     # Create a Boto3 client for AWS Organizations
+#     client = boto3.client('organizations')
+
+#     # Retrieve the list of AI service opt-out policies
+#     response = client.list_policies(Filter='AISERVICES_OPT_OUT_POLICY')
+
+#     # Extract the list of policies
+#     ai_policies = response['Policies']
+
+#     # Find the output folder path
+#     current_directory = os.getcwd()
+#     output_folder = os.path.join(current_directory, 'output', 'ai_policies')
+
+#     # Create the output folder if it doesn't exist
+#     if not os.path.exists(output_folder):
+#         os.makedirs(output_folder)
+
+#     # Get the details of each AI policy
+#     for ai_policy in ai_policies:
+#         policy_id = ai_policy['Id']
+#         policy_name = ai_policy['Name']
+
+#         # Retrieve the policy content
+#         policy_response = client.describe_policy(PolicyId=policy_id)
+#         policy_content_str = policy_response['Policy']['Content']
+
+#         # Create a separate JSON file for each AI policy in the output folder
+#         policy_file_name = os.path.join(output_folder, f"{policy_name}.json")
+
+#         # Load the policy content as JSON to format it
+#         policy_content = json.loads(policy_content_str)
+
+#         # Write the formatted policy content to the JSON file
+#         with open(policy_file_name, 'w') as file:
+#             json.dump(policy_content, file, indent=4)
+
+#         print(f"JSON file generated for policy '{policy_name}'.")
+
 import boto3
 import json
+import os
 
 def list_ai_policies():
     # Create a Boto3 client for AWS Organizations
@@ -11,8 +96,13 @@ def list_ai_policies():
     # Extract the list of policies
     ai_policies = response['Policies']
 
-    # Specify the output folder where JSON files will be stored
-    output_folder = '/home/sahil/Documents/All_detail/output/Policies/ai_policies'
+    # Find the output folder path
+    current_directory = os.getcwd()
+    output_folder = os.path.join(current_directory, 'output', 'Policies', 'ai_policies')
+
+    # Create the output folder if it doesn't exist
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
 
     # Get the details of each AI policy
     for ai_policy in ai_policies:
@@ -24,7 +114,7 @@ def list_ai_policies():
         policy_content_str = policy_response['Policy']['Content']
 
         # Create a separate JSON file for each AI policy in the output folder
-        policy_file_name = f"{output_folder}/{policy_name}.json"
+        policy_file_name = os.path.join(output_folder, f"{policy_name}.json")
 
         # Load the policy content as JSON to format it
         policy_content = json.loads(policy_content_str)
@@ -34,5 +124,4 @@ def list_ai_policies():
             json.dump(policy_content, file, indent=4)
 
         print(f"JSON file generated for policy '{policy_name}'.")
-
 
--- EnabledServices/enable_service.py
+++ EnabledServices/enable_service.py
@@ -1,33 +1,3 @@
-# import boto3
-# import json
-
-# def list_enabled_services():
-#     org_client = boto3.client('organizations')
-
-#     # Call the ListAWSServiceAccessForOrganization API to retrieve the enabled services
-#     list_services_output = org_client.list_aws_service_access_for_organization()
-
-#     # Extract the enabled services from the response
-#     enabled_services = list_services_output['EnabledServicePrincipals']
-
-#     # Create a list to store the enabled services
-#     enabled_service_list = []
-
-#     # Iterate over the enabled services and extract the service principal
-#     for service in enabled_services:
-#         service_principal = service['ServicePrincipal']
-#         enabled_service_list.append(service_principal)
-
-#     # Write the enabled services to a JSON file
-#     with open('enabled_services.json', 'w') as file:
-        
-#         json.dump(enabled_service_list, file, indent=4)
-
-#     print("JSON file generated successfully with enabled services.")
-
-
-
-# list_enabled_services()
 
 # import boto3
 # import json
@@ -55,16 +25,19 @@
 #     # Convert datetime objects to string representation
 #     data_serialized = json.dumps(data, indent=4, default=serialize_datetime)
 
-#     # Write the enabled services to a JSON file
-#     with open('enabled_services.json', 'w') as file:
+#     # Specify the path to the output folder and file name
+#     output_folder = '/home/sahil/Documents/All_detail/output/Service_enabled'
+#     output_file = f'{output_folder}/enabled_services.json'
+
+#     # Write the enabled services to a JSON file in the specified folder
+#     with open(output_file, 'w') as file:
 #         file.write(data_serialized)
 
 #     print("JSON file generated successfully with enabled services.")
 
-
-
 import boto3
 import json
+import os
 from datetime import datetime
 
 def serialize_datetime(obj):
@@ -89,9 +62,25 @@ def list_enabled_services():
     # Convert datetime objects to string representation
     data_serialized = json.dumps(data, indent=4, default=serialize_datetime)
 
-    # Specify the path to the output folder and file name
-    output_folder = '/home/sahil/Documents/All_detail/output/Service_enabled'
-    output_file = f'{output_folder}/enabled_services.json'
+    # Find the output folder path
+    current_directory = os.getcwd()
+    output_folder = None
+    for root, dirs, files in os.walk(current_directory):
+        if 'output' in dirs:
+            output_folder = os.path.join(root, 'output')
+            break
+
+    # Create the output folder if it doesn't exist
+    if output_folder is None:
+        raise Exception("Output folder not found.")
+
+    # Specify the output file path
+    service_enabled_folder = os.path.join(output_folder, 'Service_enabled')
+    output_file = os.path.join(service_enabled_folder, 'enabled_services.json')
+
+    # Create the Service_enabled folder if it doesn't exist
+    if not os.path.exists(service_enabled_folder):
+        os.makedirs(service_enabled_folder)
 
     # Write the enabled services to a JSON file in the specified folder
     with open(output_file, 'w') as file:
