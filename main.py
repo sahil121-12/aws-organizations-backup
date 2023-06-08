6158e2bf6ebffc2647d7dcb560f6c449c255cc97
sahil1202-12 <sahilsahni1819@gmail.com>
2023-06-08 23:35:46 +0530

    first commit

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from EnabledAccounts import acc_detail
-from EnabledPolices import ai_policies,backup_policies,SCP_pol_detail,tag_policies
-from EnabledServices import enable_service
+from EnabledAccounts import accountDetail
+from EnabledPolices import aiPolicies,backupPolicies,scpPolicies,tagPolicies
+from EnabledServices import enableService
 
-enable_service.list_enabled_services()
-acc_detail.get_account_details()
+enableService.list_enabled_services()
+accountDetail.get_account_details()
 
-ai_policies.list_ai_policies()
-backup_policies.list_backup_policies()
-SCP_pol_detail.list_scp_policies()
-tag_policies.list_tag_policies()
+aiPolicies.list_ai_policies()
+backupPolicies.list_backup_policies()
+scpPolicies.list_scp_policies()
+tagPolicies.list_tag_policies()
 
 
