52db5e474c678caa6e86afe923e59c8fecd27bc7
sahil1202-12 <sahilsahni1819@gmail.com>
2023-06-12 12:08:38 +0530

    done

--- EnabledServices/enableService.py
+++ EnabledServices/enableService.py
@@ -1,12 +1,168 @@
+# import boto3
+# import json
+# import os
+# from datetime import datetime
+
+# def serialize_datetime(obj):
+#     if isinstance(obj, datetime):
+#         return obj.isoformat()
+#     raise TypeError(f"Object of type {obj.__class__.__name__} is not JSON serializable")
+
+# def list_enabled_services():
+#     org_client = boto3.client('organizations')
+
+#     # Call the ListAWSServiceAccessForOrganization API to retrieve the enabled services
+#     list_services_output = org_client.list_aws_service_access_for_organization()
+
+#     # Extract the enabled services from the response
+#     enabled_services = list_services_output['EnabledServicePrincipals']
+
+#     # Create a dictionary to store the enabled services
+#     data = {
+#         'EnabledServices': enabled_services
+#     }
+
+#     # Convert datetime objects to string representation
+#     data_serialized = json.dumps(data, indent=4, default=serialize_datetime)
+
+#     # Find the output folder path
+#     current_directory = os.getcwd()
+#     output_folder = None
+#     for root, dirs, files in os.walk(current_directory):
+#         if 'output' in dirs:
+#             output_folder = os.path.join(root, 'output')
+#             break
+
+#     # Create the output folder if it doesn't exist
+#     if output_folder is None:
+#         raise Exception("Output folder not found.")
+
+#     # Specify the output file path
+#     service_enabled_folder = os.path.join(output_folder, 'Service_enabled')
+#     output_file = os.path.join(service_enabled_folder, 'enabled_services.json')
+
+#     # Create the Service_enabled folder if it doesn't exist
+#     if not os.path.exists(service_enabled_folder):
+#         os.makedirs(service_enabled_folder)
+
+#     # Write the enabled services to a JSON file in the specified folder
+#     with open(output_file, 'w') as file:
+#         file.write(data_serialized)
+
+#     print("JSON file generated successfully with enabled services.")
+
+
+
+
+# import boto3
+# import json
+# import os
+# from datetime import datetime
+
+# def serialize_datetime(obj):
+#     if isinstance(obj, datetime):
+#         return obj.isoformat()
+#     raise TypeError(f"Object of type {obj.__class__.__name__} is not JSON serializable")
+
+# def list_enabled_services():
+#     org_client = boto3.client('organizations')
+
+#     # Call the ListAWSServiceAccessForOrganization API to retrieve the enabled services
+#     list_services_output = org_client.list_aws_service_access_for_organization()
+
+#     # Extract the enabled services from the response
+#     enabled_services = list_services_output['EnabledServicePrincipals']
+
+#     # Create a list to store the enabled services with 1:1 mapping
+#     data = []
+#     for service in enabled_services:
+#         service_data = {'Service': service}
+#         data.append(service_data)
+
+#     # Convert datetime objects to string representation
+#     data_serialized = json.dumps(data, indent=4, default=serialize_datetime)
+
+#     # Find the output folder path
+#     current_directory = os.getcwd()
+#     output_folder = None
+#     for root, dirs, files in os.walk(current_directory):
+#         if 'output' in dirs:
+#             output_folder = os.path.join(root, 'output')
+#             break
+
+#     # Create the output folder if it doesn't exist
+#     if output_folder is None:
+#         raise Exception("Output folder not found.")
+
+#     # Specify the output file path
+#     service_enabled_folder = os.path.join(output_folder, 'Service_enabled')
+#     output_file = os.path.join(service_enabled_folder, 'enabled_services.json')
+
+#     # Create the Service_enabled folder if it doesn't exist
+#     if not os.path.exists(service_enabled_folder):
+#         os.makedirs(service_enabled_folder)
+
+#     # Write the enabled services to a JSON file in the specified folder
+#     with open(output_file, 'w') as file:
+#         file.write(data_serialized)
+
+#     print("JSON file generated successfully with enabled services.")
+
+
+
+
+
+# import boto3
+# import json
+# import os
+
+# def list_enabled_services():
+#     org_client = boto3.client('organizations')
+
+#     # Call the ListAWSServiceAccessForOrganization API to retrieve the enabled services
+#     list_services_output = org_client.list_aws_service_access_for_organization()
+
+#     # Extract the enabled services from the response
+#     enabled_services = [service['ServicePrincipal'] for service in list_services_output['EnabledServicePrincipals']]
+
+#     # Create a dictionary to store the enabled services
+#     data = {
+#         'EnabledServices': enabled_services
+#     }
+
+#     # Convert the dictionary to JSON format
+#     data_serialized = json.dumps(data, indent=4)
+
+#     # Find the output folder path
+#     current_directory = os.getcwd()
+#     output_folder = None
+#     for root, dirs, files in os.walk(current_directory):
+#         if 'output' in dirs:
+#             output_folder = os.path.join(root, 'output')
+#             break
+
+#     # Create the output folder if it doesn't exist
+#     if output_folder is None:
+#         raise Exception("Output folder not found.")
+
+#     # Specify the output file path
+#     service_enabled_folder = os.path.join(output_folder, 'Service_enabled')
+#     output_file = os.path.join(service_enabled_folder, 'enabled_services.json')
+
+#     # Create the Service_enabled folder if it doesn't exist
+#     if not os.path.exists(service_enabled_folder):
+#         os.makedirs(service_enabled_folder)
+
+#     # Write the enabled services to a JSON file in the specified folder
+#     with open(output_file, 'w') as file:
+#         file.write(data_serialized)
+
+#     print("JSON file generated successfully with enabled services.")
+
+
 import boto3
 import json
 import os
-from datetime import datetime
-
-def serialize_datetime(obj):
-    if isinstance(obj, datetime):
-        return obj.isoformat()
-    raise TypeError(f"Object of type {obj.__class__.__name__} is not JSON serializable")
 
 def list_enabled_services():
     org_client = boto3.client('organizations')
@@ -14,16 +170,23 @@ def list_enabled_services():
     # Call the ListAWSServiceAccessForOrganization API to retrieve the enabled services
     list_services_output = org_client.list_aws_service_access_for_organization()
 
-    # Extract the enabled services from the response
+    # Extract the enabled services and their details from the response
     enabled_services = list_services_output['EnabledServicePrincipals']
 
-    # Create a dictionary to store the enabled services
+    # Create a list to store the enabled service names
+    service_names = []
+
+    # Populate the list with the service names
+    for service in enabled_services:
+        service_names.append(service['ServicePrincipal'])
+
+    # Create a dictionary to store the enabled services with their details
     data = {
-        'EnabledServices': enabled_services
+        'EnabledServices': service_names
     }
 
-    # Convert datetime objects to string representation
-    data_serialized = json.dumps(data, indent=4, default=serialize_datetime)
+    # Convert the dictionary to JSON format
+    data_serialized = json.dumps(data, indent=4)
 
     # Find the output folder path
     current_directory = os.getcwd()
